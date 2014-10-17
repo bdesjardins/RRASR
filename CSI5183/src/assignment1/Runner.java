e0e20979626aa0996f917925ef70b82669f12801
Ben <b-desjardins@live.ca>
2014-10-17 16:56:32 -0400

    SimpleMax problem completion

--- CSI5183/src/assignment1/Runner.java
+++ CSI5183/src/assignment1/Runner.java
@@ -1,15 +1,74 @@
 package assignment1;
 
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
-		int generations = 50;
-		int popSize = 150;
+		Scanner input = new Scanner(System.in);
 		
-		BasicGA genetic = new BasicGA(new OneMaxSolution());
+		System.out.println("Please select problem: 1 for OneMax, 2 for SimpleMax");		
+		int problem = input.nextInt();
+		
+		if (problem == 3) {
+			System.out.println("Running SimpleMax");
+			System.out.println("-----------------------------------------------------------------");
+			runSimpleMax(50, 1, 0.005, 0.70);
+			
+			input.close();			
+			System.exit(0);
+		}
+		
+		System.out.println("Please input a population size:");		
+		int popSize = input.nextInt();
+		
+		System.out.println("Please input how many generations to run:");		
+		int generations = input.nextInt();
+		
+		System.out.println("Please input a mutation chance (0.001):");		
+		double mutationChance = input.nextDouble();
+		
+		System.out.println("Input a fitness reproduction chance (0.8):");		
+		double reproductionChance = input.nextDouble();
+		
+		System.out.println("");
+				
+//		int generations = 50;
+//		int popSize = 150;
+		
+		if (problem == 1) {
+			System.out.println("Running OneMax");
+			System.out.println("-----------------------------------------------------------------");
+			
+			//runOneMax(100, 50, 0.001, 0.8);
+			runOneMax(popSize, generations, mutationChance, reproductionChance);
+		} else if (problem ==2) {
+			System.out.println("Running SimpleMax");
+			System.out.println("-----------------------------------------------------------------");
+			//runSimpleMax(50, 1, 0.005, 0.70);
+			runSimpleMax(popSize, generations, mutationChance, reproductionChance);
+		} else {
+			System.out.println("Selected problem doesn't exist. GET OUT NOW");
+		}
+		
+		input.close();
+	}
+	
+	private static void runOneMax(int popSize, int generations, double mutation, double reproduction) {
+		BasicGA genetic = new BasicGA(new OneMaxSolution(), mutation, reproduction);
 		
 		for (int i = 0; i < popSize; i++) {
 			genetic.addSolution(new OneMaxSolution());
+		}
+		
+		genetic.runGeneration(generations);
+	}
+	
+	private static void runSimpleMax(int popSize, int generations, double mutation, double reproduction) {
+		BasicGA genetic = new BasicGA(new SimpleMaxSolution(), mutation, reproduction);
+		
+		for (int i = 0; i < popSize; i++) {
+			genetic.addSolution(new SimpleMaxSolution());
 		}
 		
 		genetic.runGeneration(generations);
---
+++ CSI5183/src/assignment1/SimpleMaxSolution.java
@@ -0,0 +1,79 @@
+package assignment1;
+
+public class SimpleMaxSolution implements Solution {
+
+	private int[] rep; //representation. Used "rep" for brevity in code
+	private double fitness;
+	
+	
+	public SimpleMaxSolution() {
+		this.rep = new int[]{getRand(),getRand(),getRand(),getRand(),getRand(),getRand(),getRand(),getRand(),getRand(),getRand()};
+	}
+	
+	public SimpleMaxSolution(int[] rep) {
+		this.rep = rep;
+		evaluate();
+	}
+	
+	@Override
+	public double getFitness() {
+		return this.fitness;
+	}
+
+	@Override
+	public void printRepresentation() {
+		System.out.println("(" + rep[0] + "*" + rep[1] + "*" + rep[2] + "*" + rep[3] + "*" + rep[4] + ")/(" +
+				rep[5] + "*" + rep[6] + "*" + rep[7] + "*" + rep[8] + "*" + rep[9] + ")" + "	" + this.fitness);
+
+	}
+
+	@Override
+	public void evaluate() {
+		double fit = (rep[0] * rep[1] * rep[2] * rep[3] * rep[4]) / (rep[5] * rep[6] * rep[7] * rep[8] * rep[9]);
+		this.fitness = fit;
+	}
+
+	@Override
+	public void mutate(double mutationChance) {
+		//Using a random replacement mutation		
+		for (int i = 0; i < rep.length; i++) {
+			if (Math.random() < mutationChance) {
+				rep[i] = getRand();
+			}
+		}
+	}
+	
+//	@Override
+//	public void mutate(double mutationChance) {
+//		//Using a swap mutation
+//		if (Math.random() < mutationChance) {
+//			int i =  (int)Math.floor((Math.random() * 10));
+//			int j =  (int)Math.floor((Math.random() * 10));
+//			
+//			int temp = rep[i];
+//			rep[i] = rep[j];
+//			rep[j] = temp;
+//		}
+//	}
+
+	@Override
+	public Solution crossover(Solution parent2) {
+		//Double point crossover: a b c d / e f g h i / j
+		
+		int[] rep2 = (int[]) parent2.getRepresentation();
+		
+		int[] child = new int[]{rep[0],rep[1],rep[2],rep[3],rep2[4],rep2[5],rep2[6],rep2[7],rep2[8],rep[9]};
+		
+		return new SimpleMaxSolution(child);
+	}
+
+	@Override
+	public Object getRepresentation() {
+		return rep;
+	}
+	
+	private int getRand() {
+		return (int) Math.floor((Math.random() * 10) + 1);
+	}
+
+}
