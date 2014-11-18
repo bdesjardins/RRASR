89328fe58129cbff7bca1c5f42ee82b7bb766de2
Ben <b-desjardins@live.ca>
2014-11-18 16:58:44 -0500

    End of day commit. Work done on getting the back-end of the GUI working

--- CSI5183/projectSrc/project/visualization/ExperimentalApplication.java
+++ CSI5183/projectSrc/project/visualization/ExperimentalApplication.java
@@ -3,34 +3,45 @@ package project.visualization;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import java.awt.BorderLayout;
-import javax.swing.BoxLayout;
-import java.awt.GridLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
+
 import java.awt.Font;
-import javax.swing.JSpinner;
+
 import javax.swing.JSlider;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.SpinnerListModel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+
 import org.eclipse.wb.swing.FocusTraversalOnArray;
+
 import java.awt.Component;
+
 import javax.swing.JComboBox;
-import javax.swing.border.BevelBorder;
 import javax.swing.JTree;
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.JProgressBar;
 import javax.swing.JButton;
-import javax.swing.border.MatteBorder;
-import java.awt.Color;
+
+import java.io.File;
+
+import javax.swing.JScrollPane;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
 
 public class ExperimentalApplication {
 
+	//UI Components
 	private JFrame frmCsiProject;
 	private JTextField populationField;
 	private JTextField generationsField;
@@ -40,6 +51,13 @@ public class ExperimentalApplication {
 	private JTextField genDistField;
 	private JTextField paretoErrorField;
 	private JTextField runtimeField;
+	private FileTreeModel model;
+	
+	
+	//Execution variables
+	private File instance;
+	private int population = 200;
+	private int generations = 500;
 
 	/**
 	 * Launch the application.
@@ -61,6 +79,8 @@ public class ExperimentalApplication {
 	 * Create the application.
 	 */
 	public ExperimentalApplication() {
+		model = new FileTreeModel(new File("C:/Users/ben/git/CSI5183_F2014/CSI5183/Instances"));
+		
 		initialize();
 	}
 
@@ -70,20 +90,15 @@ public class ExperimentalApplication {
 	private void initialize() {
 		frmCsiProject = new JFrame();
 		frmCsiProject.setTitle("CSI5183 - Project");
-		frmCsiProject.setBounds(100, 100, 930, 700);
+		frmCsiProject.setBounds(100, 100, 1112, 700);
 		frmCsiProject.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(192, 36, 525, 580);
 		
 		JLabel lblExperimentalRunner = new JLabel("RRASR Experiment Runner");
-		lblExperimentalRunner.setBounds(343, 0, 227, 23);
 		lblExperimentalRunner.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		frmCsiProject.getContentPane().setLayout(null);
 		
 		JPanel parametersPanel = new JPanel();
-		parametersPanel.setBounds(10, 36, 172, 580);
-		frmCsiProject.getContentPane().add(parametersPanel);
 		
 		JComboBox algorithmSpinner = new JComboBox();
 		algorithmSpinner.setModel(new DefaultComboBoxModel(new String[] {"NSGAII", "NSGAIII", "SPEA2", "IBEA"}));
@@ -97,6 +112,33 @@ public class ExperimentalApplication {
 		lblProblemInstance.setFont(new Font("Tahoma", Font.BOLD, 12));
 		
 		populationField = new JTextField();
+		populationField.getDocument().addDocumentListener(new DocumentListener() {
+			public void changedUpdate(DocumentEvent e) {
+				warn();
+			}
+			public void removeUpdate(DocumentEvent e) {
+				warn();
+			}
+			public void insertUpdate(DocumentEvent e) {
+				warn();
+			}
+
+			public void warn() {
+				try {
+					if (Integer.parseInt(populationField.getText())<=0){
+						JOptionPane.showMessageDialog(null,
+								"Error: Please enter number bigger than 0", "Error Message",
+								JOptionPane.ERROR_MESSAGE);
+					} else {
+						setPopulation(Integer.parseInt(populationField.getText()));
+					}
+				} catch (NumberFormatException e) {
+					JOptionPane.showMessageDialog(null,
+							"Error: Please enter number bigger than 0", "Error Message",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
 		populationField.setText("200");
 		populationField.setColumns(10);
 		
@@ -109,56 +151,47 @@ public class ExperimentalApplication {
 		lblOfGenerations.setFont(new Font("Tahoma", Font.BOLD, 12));
 		
 		generationsField = new JTextField();
+		generationsField.getDocument().addDocumentListener(new DocumentListener() {
+			public void changedUpdate(DocumentEvent e) {
+				warn();
+			}
+			public void removeUpdate(DocumentEvent e) {
+				warn();
+			}
+			public void insertUpdate(DocumentEvent e) {
+				warn();
+			}
+
+			public void warn() {
+				try {
+					if (Integer.parseInt(generationsField.getText())<=0){
+						JOptionPane.showMessageDialog(null,
+								"Error: Please enter number bigger than 0", "Error Message",
+								JOptionPane.ERROR_MESSAGE);
+					} else {
+						setGenerations(Integer.parseInt(generationsField.getText()));
+					}
+				} catch (NumberFormatException e) {
+					JOptionPane.showMessageDialog(null,
+							"Error: Please enter number bigger than 0", "Error Message",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
 		generationsField.setText("500");
 		generationsField.setColumns(10);
+				
+		JScrollPane scrollPane = new JScrollPane();
 		
-		JTree instanceSelector = new JTree();
-		instanceSelector.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
-		instanceSelector.setRootVisible(false);
-		GroupLayout gl_parametersPanel = new GroupLayout(parametersPanel);
-		gl_parametersPanel.setHorizontalGroup(
-			gl_parametersPanel.createParallelGroup(Alignment.LEADING)
-				.addComponent(lblProblemInstance, GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE)
-				.addGroup(Alignment.TRAILING, gl_parametersPanel.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_parametersPanel.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblAlgorithm, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
-						.addComponent(populationField, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
-						.addComponent(algorithmSpinner, 0, 152, Short.MAX_VALUE)
-						.addComponent(lblPopulationSize, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
-						.addComponent(generationsField, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
-						.addComponent(lblOfGenerations, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE))
-					.addContainerGap())
-				.addGroup(gl_parametersPanel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(instanceSelector, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
-					.addContainerGap())
-		);
-		gl_parametersPanel.setVerticalGroup(
-			gl_parametersPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_parametersPanel.createSequentialGroup()
-					.addComponent(lblProblemInstance, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(instanceSelector, GroupLayout.PREFERRED_SIZE, 384, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(lblAlgorithm, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
-					.addGap(2)
-					.addComponent(algorithmSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(lblPopulationSize)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(populationField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(lblOfGenerations, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(generationsField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap())
-		);
-		parametersPanel.setLayout(gl_parametersPanel);
+		JTree tree = new JTree(model);
+		tree.setRootVisible(false);
+		
+		tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		tree.addTreeSelectionListener(new SelectionListener());
+		
+		scrollPane.setViewportView(tree);
 		
 		JPanel resultsPanel = new JPanel();
-		resultsPanel.setBounds(727, 36, 172, 580);
-		frmCsiProject.getContentPane().add(resultsPanel);
 		
 		JLabel lblHypervolume = new JLabel("Hypervolume");
 		lblHypervolume.setHorizontalAlignment(SwingConstants.CENTER);
@@ -222,7 +255,7 @@ public class ExperimentalApplication {
 		genNumberLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		GroupLayout gl_resultsPanel = new GroupLayout(resultsPanel);
 		gl_resultsPanel.setHorizontalGroup(
-			gl_resultsPanel.createParallelGroup(Alignment.LEADING)
+			gl_resultsPanel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_resultsPanel.createSequentialGroup()
 					.addGroup(gl_resultsPanel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_resultsPanel.createSequentialGroup()
@@ -270,15 +303,15 @@ public class ExperimentalApplication {
 					.addContainerGap(31, Short.MAX_VALUE))
 				.addGroup(gl_resultsPanel.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(genSlider, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
+					.addComponent(genSliderLabel, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
 					.addContainerGap())
 				.addGroup(gl_resultsPanel.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(genSliderLabel, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
-					.addContainerGap())
-				.addGroup(Alignment.TRAILING, gl_resultsPanel.createSequentialGroup()
-					.addContainerGap()
 					.addComponent(genNumberLabel, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(Alignment.LEADING, gl_resultsPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(genSlider, GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
 					.addContainerGap())
 		);
 		gl_resultsPanel.setVerticalGroup(
@@ -321,7 +354,6 @@ public class ExperimentalApplication {
 					.addGap(23))
 		);
 		resultsPanel.setLayout(gl_resultsPanel);
-		frmCsiProject.getContentPane().add(tabbedPane);
 		
 		JPanel graphTab = new JPanel();
 		tabbedPane.addTab("Pareto Set Viewer", null, graphTab, null);
@@ -330,19 +362,123 @@ public class ExperimentalApplication {
 		tabbedPane.addTab("Solution Viewer", null, tourTab, null);
 		tourTab.setLayout(null);
 		
-		JPanel tourPane = new JPanel();
-		tourPane.setBounds(10, 11, 500, 500);
-		tourTab.add(tourPane);
+//		TourVisualizer tourPane = new TourVisualizer();
+//		tourPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+//		tourPane.setBounds(10, 11, 500, 500);
+//		tourTab.add(tourPane);
 		
 		JLabel lblNewLabel_5 = new JLabel("Solution:");
 		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_5.setBounds(10, 527, 66, 14);
+//		lblNewLabel_5.setBounds(10, 527, 66, 14);
 		tourTab.add(lblNewLabel_5);
 		
 		JComboBox solutionSelection = new JComboBox();
-		solutionSelection.setBounds(86, 522, 424, 20);
+//		solutionSelection.setBounds(86, 522, 424, 20);
 		tourTab.add(solutionSelection);
-		frmCsiProject.getContentPane().add(lblExperimentalRunner);
+		GroupLayout gl_parametersPanel = new GroupLayout(parametersPanel);
+		gl_parametersPanel.setHorizontalGroup(
+			gl_parametersPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_parametersPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(12, Short.MAX_VALUE))
+				.addComponent(lblProblemInstance, GroupLayout.DEFAULT_SIZE, 362, Short.MAX_VALUE)
+				.addGroup(gl_parametersPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblAlgorithm, GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(gl_parametersPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblPopulationSize, GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(Alignment.TRAILING, gl_parametersPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblOfGenerations, GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(gl_parametersPanel.createSequentialGroup()
+					.addGap(99)
+					.addComponent(populationField, GroupLayout.PREFERRED_SIZE, 152, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(111, Short.MAX_VALUE))
+				.addGroup(gl_parametersPanel.createSequentialGroup()
+					.addGap(98)
+					.addComponent(generationsField, GroupLayout.PREFERRED_SIZE, 152, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(112, Short.MAX_VALUE))
+				.addGroup(gl_parametersPanel.createSequentialGroup()
+					.addGap(101)
+					.addComponent(algorithmSpinner, GroupLayout.PREFERRED_SIZE, 152, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(109, Short.MAX_VALUE))
+		);
+		gl_parametersPanel.setVerticalGroup(
+			gl_parametersPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_parametersPanel.createSequentialGroup()
+					.addComponent(lblProblemInstance, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 389, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblAlgorithm, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
+					.addGap(2)
+					.addComponent(algorithmSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(lblPopulationSize)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(populationField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(lblOfGenerations)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(generationsField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+		);
+		parametersPanel.setLayout(gl_parametersPanel);
+		GroupLayout groupLayout = new GroupLayout(frmCsiProject.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(343)
+							.addComponent(lblExperimentalRunner))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(parametersPanel, GroupLayout.PREFERRED_SIZE, 362, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(tabbedPane, GroupLayout.PREFERRED_SIZE, 525, GroupLayout.PREFERRED_SIZE)
+							.addGap(6)
+							.addComponent(resultsPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addGap(99))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(lblExperimentalRunner)
+					.addGap(13)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(resultsPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(parametersPanel, GroupLayout.PREFERRED_SIZE, 580, GroupLayout.PREFERRED_SIZE)
+						.addComponent(tabbedPane, GroupLayout.PREFERRED_SIZE, 580, GroupLayout.PREFERRED_SIZE))
+					.addGap(46))
+		);
+		frmCsiProject.getContentPane().setLayout(groupLayout);
 		frmCsiProject.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{tabbedPane, lblExperimentalRunner}));
+	}
+
+	private class SelectionListener implements TreeSelectionListener {
+		@Override
+		public void valueChanged(TreeSelectionEvent se) {
+			JTree tree = (JTree) se.getSource();
+			File selectedNode = (File) tree.getLastSelectedPathComponent();
+			if (selectedNode.isFile()) {
+				setInstance((File) selectedNode);
+			}
+		}
+	}
+	
+	private void setInstance(File newInstance) {
+		this.instance = newInstance;
+	}
+	private void setPopulation(int newPop) {
+		this.population = newPop;
+	}
+	private void setGenerations(int newGen) {
+		this.generations = newGen;
 	}
 }
--- CSI5183/projectSrc/project/visualization/TourVisualizer.java
+++ CSI5183/projectSrc/project/visualization/TourVisualizer.java
@@ -1,14 +1,142 @@
 package project.visualization;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
 import javax.swing.JPanel;
+
+import project.problem.types.NodeInfo;
 
 public class TourVisualizer extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
+	private NodeInfo[] nodes;	
+	private ArrayList<Integer> sensingHoles;	
+	private Integer[] solutionVector;
+	
+	private double nodeWidth;
+	private Insets insets;
+	
 	public TourVisualizer() {
+		super();
+		
+		nodeWidth = 4.0;
+		insets = new Insets((int)nodeWidth, (int)nodeWidth, (int)nodeWidth, (int)nodeWidth);
+		
+		setBackground(Color.WHITE);
+		setForeground(Color.BLACK);
+	}
+	
+	public void updateContent(NodeInfo[] nodes, ArrayList<Integer> sensingHoles) {
+		this.nodes = nodes;
+		this.sensingHoles = sensingHoles;
+	}
+	
+	public void reset() {
+		this.nodes = new NodeInfo[0];
+		this.sensingHoles = new ArrayList<Integer>();
+	}
+	
+	public void updateSelection(int[] tour) {
+		ArrayList<Integer> vector = new ArrayList<Integer>();
+		
+		for (int i=0; i < tour.length; i++){
+			if (tour[i] > 0) {
+				vector.add(i);
+			}
+		}
+		
+		this.solutionVector = vector.toArray(new Integer[0]);
+	}
+	
+	@Override
+	protected synchronized void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+//		if (nodes.length < 1 || solutionVector.length < 1 || sensingHoles.size() < 1) {
+//			return;
+//		}
 
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		// first determine bounds of the data
+//		boolean isGeographical = EdgeWeightType.GEO.equals(problem.getEdgeWeightType());
+		double left = Double.POSITIVE_INFINITY;
+		double right = Double.NEGATIVE_INFINITY;
+		double bottom = Double.POSITIVE_INFINITY;
+		double top = Double.NEGATIVE_INFINITY;
+
+		for (int i = 1; i <= nodes.length; i++) {
+			NodeInfo node = nodes[i];
+			double[] position = new double[]{node.getLocation().getX() + 500, node.getLocation().getY() + 500};
+
+			left = Math.min(left, position[0]);
+			right = Math.max(right, position[0]);
+			bottom = Math.min(bottom, position[1]);
+			top = Math.max(top, position[1]);
+		}
+
+		// calculate the bounds of the drawing
+		int displayWidth = getWidth();
+		int displayHeight = getHeight();
+		double scaleX = (displayWidth - insets.right - insets.left) / (right - left);
+		double scaleY = (displayHeight - insets.top - insets.bottom) / (top - bottom);
+		double scale = Math.min(scaleX, scaleY);
+		double offsetX = (displayWidth - insets.right - insets.left - scale * (right - left)) / 2.0;
+		double offsetY = (displayHeight - insets.top - insets.bottom - scale * (top - bottom)) / 2.0;
+
+		// draw the tours
+
+
+		g2.setPaint(Color.BLUE);
+		g2.setStroke(new BasicStroke());
+
+		for (int i = 0; i < solutionVector.length; i++) {
+			NodeInfo node1 = nodes[solutionVector[i]];
+			NodeInfo node2 = nodes[solutionVector[i+1]];;
+			double[] position1 = {node1.getLocation().getX()+ 500, node1.getLocation().getY()+ 500};
+			double[] position2 = {node2.getLocation().getX()+ 500, node2.getLocation().getY()+ 500};
+
+			Line2D line = new Line2D.Double(
+					displayWidth - (offsetX + scale * (position1[0] - left) + insets.left), 
+					displayHeight - (offsetY + scale * (position1[1] - bottom) + insets.bottom),
+					displayWidth - (offsetX + scale * (position2[0] - left) + insets.left),
+					displayHeight - (offsetY + scale * (position2[1] - bottom) + insets.bottom));
+
+			g2.draw(line);
+		}
+		
+
+		// draw the nodes
+		
+
+		for (int i = 1; i <= nodes.length; i++) {
+			g2.setColor(Color.BLACK);
+			NodeInfo node = nodes[i];
+			double[] position = {node.getLocation().getX()+ 500, node.getLocation().getY()+ 500};
+			
+			if (sensingHoles.contains(i)) {
+				g2.setColor(Color.RED);
+			}
+
+			Ellipse2D point = new Ellipse2D.Double(
+					displayWidth - (offsetX + scale * (position[0] - left) + insets.left) - (nodeWidth / 2.0),
+					displayHeight - (offsetY + scale * (position[1] - bottom) + insets.bottom) - (nodeWidth / 2.0),
+					nodeWidth,
+					nodeWidth);
+
+			g2.fill(point);
+			g2.draw(point);
+		}
 	}
 
 }
